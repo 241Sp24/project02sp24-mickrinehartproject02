158578dbad56d314a4fc511809c3272ff942a2d5
OwenMick1 <s548897@nwmissouri.edu>
2024-04-05 10:48:34 -0500

    Created and worked on Graduate student class

---
+++ src/studentdriver/GraduateStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdriver;
+
+/**
+ *
+ * @author s548897
+ */
+public class GraduateStudent extends StudentFees {
+
+    private int coursesEnrolled;
+    private boolean isGraduateAssistant;
+    private String graduateAssistantType;
+    private double getPayableAmount;
+
+    //Constructors
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, String graduateAssistantType, int coursesEnrolled) {
+        super(studentName, studentID, isEnrolled);
+        this.coursesEnrolled = coursesEnrolled;
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.graduateAssistantType = graduateAssistantType;
+        this.getPayableAmount = getPayableAmount;
+    }
+}
